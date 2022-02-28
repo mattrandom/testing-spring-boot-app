adad5b566ada14ec538698570c89e73c95100c8a
mattrandom <44298255+mattrandom@users.noreply.github.com>
2022-02-28 16:23:38 +0100

    JUnit test for fetching Employees collection

--- src/test/java/io/mattrandom/repository/EmployeeRepositoryTests.java
+++ src/test/java/io/mattrandom/repository/EmployeeRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -31,5 +33,33 @@ class EmployeeRepositoryTests {
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee).isEqualTo(employee);
         assertThat(savedEmployee.getId()).isGreaterThanOrEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("JUnit test for getting Employees collection")
+    void givenEmployeesList_whenFindAll_thenReturnEmployeesList() {
+        //given
+        Employee employee1 = Employee.builder()
+                .firstName("Matt")
+                .lastName("Random")
+                .email("test@gmail.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("Matheus")
+                .lastName("Test")
+                .email("email@gmail.com")
+                .build();
+
+        List<Employee> employees = employeeRepository.saveAll(List.of(employee1, employee2));
+
+        //when
+        List<Employee> employeesFounded = employeeRepository.findAll();
+
+        //then
+        assertThat(employeesFounded).isNotNull();
+        assertThat(employeesFounded.get(0).getFirstName()).isEqualTo(employee1.getFirstName());
+        assertThat(employeesFounded).hasSize(2);
+
     }
 }
