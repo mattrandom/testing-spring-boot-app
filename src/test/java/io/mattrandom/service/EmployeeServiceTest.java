0fb08d13b47b36d72d60e6a577cadab2a227f8e8
mattrandom <44298255+mattrandom@users.noreply.github.com>
2022-03-02 17:50:30 +0100

    JUnit test verifying it the exception is being thrown

--- src/test/java/io/mattrandom/service/EmployeeServiceTest.java
+++ src/test/java/io/mattrandom/service/EmployeeServiceTest.java
@@ -1,7 +1,9 @@
 package io.mattrandom.service;
 
+import io.mattrandom.exception.ResourceNotFoundException;
 import io.mattrandom.model.Employee;
 import io.mattrandom.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -24,15 +27,21 @@ class EmployeeServiceTest {
     @InjectMocks
     private EmployeeServiceImpl employeeService;
 
-    @Test
-    @DisplayName("JUnit test for saveEmployee method")
-    void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
-        //given
-        Employee employee = Employee.builder()
+    private Employee employee;
+
+    @BeforeEach
+    void setUp() {
+        employee = Employee.builder()
                 .firstName("Matt")
                 .lastName("Random")
                 .email("test@gmail.com")
                 .build();
+    }
+
+    @Test
+    @DisplayName("JUnit test for saveEmployee method")
+    void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
+        //given
         given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
         given(employeeRepository.save(any(Employee.class))).willReturn(employee);
 
@@ -41,6 +50,16 @@ class EmployeeServiceTest {
 
         //then
         assertThat(employeeSaved).isNotNull();
+    }
+
+    @Test
+    @DisplayName("JUnit test verifying it the exception is being thrown")
+    void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+        //given
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
+
+        //when & then
+        assertThrows(ResourceNotFoundException.class, () -> employeeService.saveEmployee(employee));
     }
 
 }
